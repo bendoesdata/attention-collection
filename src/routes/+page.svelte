863b91d6a77496bf001d76029b70e309ffc78a9c
Ben Cooley <benjamincooley94@gmail.com>
2023-01-12 11:51:58 -0500

    change title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 import Bookshelf from "../components/bookshelf.svelte"
 </script>
 <main>
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <h1>Attnetion Collection</h1>
+    <Bookshelf />
 </main>
-<Bookshelf />
+
