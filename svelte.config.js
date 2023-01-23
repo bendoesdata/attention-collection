14133b4e6a73c6ba4b015fb2b9a57a54c7ac7b33
Ben Cooley <benjamincooley94@gmail.com>
2023-01-23 16:24:34 -0500

    setup for netlify deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
+// const config = {
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
 
-export default config;
+export default {
+	kit: {
+	  adapter: adapter()
+	}
+  };
