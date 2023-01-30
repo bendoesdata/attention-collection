6c04f6d0d2f6459414c57431024dd90925952d89
Ben Cooley <benjamincooley94@gmail.com>
2023-01-30 16:32:19 -0500

    fix nav

--- src/components/menu.svelte
+++ src/components/menu.svelte
@@ -1,4 +1,16 @@
+<script>
+  import * as animateScroll from "svelte-scrollto";
+
+</script>
 <nav class="hidden text-gray-500 uppercase text-bold sm:block" style="color: yellow; background-color: #2e5b3c">
-    <a href="/about" class="hover:text-gray-700 hover:no-underline" style="padding: 10px">Articles</a>
-      <a href="/contact" class="hover:text-gray-700 hover:no-underline" style="padding: 10px">Books</a>
+    <p on:click={() => animateScroll.scrollTo({element: '#article-section', duration: 2000, offset: -600})}
+       class="hover:text-gray-700 hover:no-underline" style="padding: 10px">Articles</p>
+      <p on:click={() => animateScroll.scrollTo({element: '#article-section', duration: 2000, offset: -600})}
+         class="hover:text-gray-700 hover:no-underline" style="padding: 10px">Books</p>
   </nav>
+
+  <style>
+    p {
+      cursor: pointer;
+    }
+  </style>
